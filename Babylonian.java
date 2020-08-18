bd2bc584e2867367120c4b125307e845745a2a73
jpolacco <jpolacco@gmail.com>
2020-08-17 22:42:49 -0700

    Add files via upload

---
+++ Babylonian.java
@@ -0,0 +1,29 @@
+
+/**
+ * Write a description of class Babylonian here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Babylonian
+{   
+    
+    /**
+     * This method computes the square root of a positive number using the Babylonian algorithm.
+     * 
+     * The Babylonian algorithm to compute the square root of a positive number n is as follows:
+     * 1. Make a guess at the answer (pick n/2 as your initial guess).
+     * 2. Compute r = n / guess
+     * 3. Set guess = (guess +r)/ 2
+     * 4. Go back to step 2 for five iterations
+     * 
+     * 
+     * @param n a positive number 
+     * @return an approximation of the square root of n using the Babylonian algorithm
+     */ 
+    double squareRoot(double n){
+        // TODO
+        return 0.0; // delete this line of code
+    }
+    
+}
